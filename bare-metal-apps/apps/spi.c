fdf2263eb4d9619218e1e3d22b6b2b861d268a75
Matthias Van Parys <matthias.vanparys@altran.com>
2016-07-08 19:14:50 +0200

    Add picotcp listening server

--- bare-metal-apps/apps/spi.c
+++ bare-metal-apps/apps/spi.c
@@ -20,39 +20,159 @@ This code was written by Carlos Moratelli at Embedded System Group (GSE) at PUCR
 #include <pic32mz.h>
 #include <libc.h>
 
+#include "pico_defines.h"
+#include "pico_stack.h"
+#include "pico_ipv4.h"
+#include "pico_socket.h"
 
-volatile int32_t t2 = 0;
+#define LISTENING_PORT 80
+#define MAX_CONNECTIONS 1
+#define RX_BUF_SIZE 1536
 
-void irq_timer(){
- t2++;     
+static char rx_buf[RX_BUF_SIZE] = {0};
+static struct pico_socket *s = NULL;
+static struct pico_ip4 my_eth_addr, netmask, inaddr_any = { };
+static struct pico_device *pico_dev_eth;
+
+
+volatile unsigned int pico_ms_tick = 0;
+static uint32_t cp0_ms_ticks = CPU_SPEED/2/1000;
+
+static uint32_t calculate_ms_passed(uint32_t old_ticks, uint32_t new_ticks)
+{
+    uint32_t diff_ticks;
+    if (new_ticks >= old_ticks)
+        diff_ticks = new_ticks - old_ticks;
+    else
+        diff_ticks = 0xffffffff - (old_ticks - new_ticks);
+
+    return diff_ticks / cp0_ms_ticks;
+}
+
+void irq_timer()
+{
+    static int prev_init = 0;
+    static uint32_t prev_count = 0;
+    uint32_t cur_count = mfc0(CP0_COUNT, 0);
+
+    if (!prev_init)
+    {
+        prev_count = mfc0(CP0_COUNT, 0);
+        prev_init = 1;
+    }
+
+    /* pico_ms_tick is not 100% accurate this way but at this point it's not required
+     * currently there's a 10% accuracy loss(1000ms only produces 900 pico_ms_ticks) */
+    if (cur_count >= prev_count + cp0_ms_ticks)
+    {
+        pico_ms_tick += calculate_ms_passed(prev_count, cur_count);
+        prev_count = cur_count;
+    }
+
 }
 
 
 /* SP1 configuration */
-void setupSPI1(){
-    uint32_t rData;
-    
-    SPI1CON = 0;
-    rData=SPI1BUF;
-    SPI1BRG=0x1;
-    SPI1STATCLR=0x40;
-    SPI1CON=0x8220; // use F PB /4 clock frequency
+static void setupSPI1()
+{
+#if 0
+    /* Pin settings */
+
+    ANSELBCLR = 0x0008; /* pin B3 used as output for CS */
+    TRISBCLR = 0x0008;
+    TRISDCLR = 0x0002;  /* pin D1 used as output for SCLK */
+    TRISFCLR = 0x0020;  /* pin F5 used as output for MOSI */
+    TRISFSET = 0x0010;  /* pin F4 used as input for MISO */
+    LATFSET = 0x0010;
+
+    SDI1R = 2;  /* pin F4 as SPI1 data input */
+    RPF5R = 5;  /* pin F5 as SPI1 data output */
+
+    /* SPI config settings */
+#endif
+    SPI1BRG = 4; /* Set clock divider to selected_clock/10: selected_clk/(2*(4+1)) */
+    SPI1CON = 0x8120; /* enable SPI / master mode / data transition from high to low clk */
+
 }
 
-int main() {
+static void cb_tcp(uint16_t ev, struct pico_socket *sock)
+{
+    int r = 0;
+    uint16_t port;
+    uint16_t peer;
+
+    if (ev & PICO_SOCK_EV_RD) {
+        r = pico_socket_read(s, rx_buf, RX_BUF_SIZE);
+        if (r < 0)
+            printf("Error while reading from socket!\n");
+        int i;
+        printf("Data received(%d bytes):\n", r);
+        for (i = 0; i < r; i++)
+            printf("%02x ", rx_buf[i]);
+        printf("\n");
+    }
+
+    if (ev == PICO_SOCK_EV_CONN)
+    {
+        s = pico_socket_accept(s, &peer, &port);
+    }
+
+    /* process error event, socket error occured */
+    if (ev == PICO_SOCK_EV_ERR)
+        printf("Socket error!\n");
+}
+
+int main()
+{
+    uint8_t mac[6] = {0x00,0x00,0x00,0x12,0x34,0x56};
+    const char *ipaddr="192.168.43.42";
+    uint16_t port_be = 0;
+
     /* Select output serial 2 = UART2, 6 = UART6 */
     serial_select(UART2);
-    
-    printf("\nConfiguring SPI.");
-    
 //    setupSPI1();
-    
-    while (1){
-        printf("\nInt count: %d", t2);
+#if 0
+    while(1)
+    {
+        printf("pico_ms_tick = %u\n", pico_ms_tick);
         udelay(1000000);
-        SPI1BUF='A';  
-   }
-    
+    }
+#endif
+    printf("Configuring SPI1\n");
+
+    setupSPI1();
+
+    printf("Configured SPI1\n");
+
+    printf("Initializing pico stack\n");
+    pico_stack_init();
+
+    printf("Creating ethernet device\n");
+    pico_dev_eth = (struct pico_device *) pico_eth_create("eth", mac);
+    if (!pico_dev_eth)
+        printf("Failed to create ethernet device!\n");
+
+    pico_string_to_ipv4(ipaddr, &my_eth_addr.addr);
+    pico_string_to_ipv4("255.255.255.0", &netmask.addr);
+    pico_ipv4_link_add(pico_dev_eth, my_eth_addr, netmask);
+
+    port_be = short_be(LISTENING_PORT);
+
+    printf("Openeing socket\n");
+    s = pico_socket_open(PICO_PROTO_IPV4, PICO_PROTO_TCP, &cb_tcp);
+    if (!s)
+        printf("Failed to open socket!\n");
+
+    if (pico_socket_bind(s, &inaddr_any, &port_be) != 0)
+        printf("Failed to bind socket!\n");
+
+    if (pico_socket_listen(s, MAX_CONNECTIONS) != 0)
+        printf("Failed to listen on socket!\n");
+
+    while (1)
+    {
+	    pico_stack_tick();
+    }
+
     return 0;
 }
-
