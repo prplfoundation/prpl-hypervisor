d950656d0e19b725e68faf02c68c75fc0e5504e7
crmoratelli <carlos.moratelli@pucrs.br>
2016-09-21 15:49:05 -0300

    Optimize the HEAP size.

--- sys/lib/malloc.c
+++ sys/lib/malloc.c
@@ -141,8 +141,8 @@ void *realloc(void *ptr, uint32_t size){
 uint32_t init_mem() {
   
   //Initialize heap           
-  HeapInit((void*)(( (uint32_t)(&_heap_start) + 4096) & 0xFFFFF000), ((int)(&_heap_size) & 0xFFFFF000));    
-  
-  return 0;
+    HeapInit((void*)(( (uint32_t)(&_heap_start) + 8) & 0xFFFFFFFC), ((uint32_t)(&_heap_size) -8) & 0xFFFFFFFC);  
+
+    return 0;
 }
 
