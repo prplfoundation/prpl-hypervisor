a18fdb2018842c1a39814e3a46f3b587549a1efb
crmoratelli <carlos.moratelli@pucrs.br>
2016-10-04 13:45:16 -0300

    Refactoring on the bare-metal compilation scheme.

--- bare-metal-apps/apps/outputUART/app.mk
+++ bare-metal-apps/apps/outputUART/app.mk
@@ -1,5 +1,17 @@
+# Define your additional include paths
+#INC_DIRS = 
+
+#Aditional C flags
+#CFLAGS = 
+
+#Aditional Libraries
+#LIBS=
+
+#default stack size 512 bytes
+STACK_SIZE = 512
+
+#Include your additional mk files here. 
 
 app:
-	$(CC) $(CFLAGS) $(INC_DIRS) \
-		$(TOPDIR)apps/$(APP)/$(APP).c 
+	$(CC) $(CFLAGS) $(INC_DIRS) $(TOPDIR)apps/$(APP)/$(APP).c -o $(TOPDIR)apps/$(APP)/$(APP).o
 
--- bare-metal-apps/lib/lib.mk
+++ bare-metal-apps/lib/lib.mk
@@ -1,9 +1,8 @@
 
 lib:
-	$(CC) $(CFLAGS) $(INC_DIRS) \
-	        $(TOPDIR)lib/libc.c \
-	        $(TOPDIR)lib/malloc.c \
-	        $(TOPDIR)lib/network.c \
-	        $(TOPDIR)lib/usb_lib.c
+	$(CC) $(CFLAGS) $(INC_DIRS) $(TOPDIR)lib/libc.c -o $(TOPDIR)apps/$(APP)/libc.o
+	$(CC) $(CFLAGS) $(INC_DIRS) $(TOPDIR)lib/malloc.c -o $(TOPDIR)apps/$(APP)/malloc.o
+	$(CC) $(CFLAGS) $(INC_DIRS) $(TOPDIR)lib/network.c -o $(TOPDIR)apps/$(APP)/network.o
+	$(CC) $(CFLAGS) $(INC_DIRS) $(TOPDIR)lib/usb_lib.c -o $(TOPDIR)apps/$(APP)/usb_lib.o
 
 
