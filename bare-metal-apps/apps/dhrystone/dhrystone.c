ee1bf55c0ecb3e5cf21513290967a8f58bd70c42
crmoratelli <carlos.moratelli@pucrs.br>
2016-11-17 17:05:53 -0200

    New application to perform dhrystone benchmark.

---
+++ bare-metal-apps/apps/dhrystone/dhrystone.c
@@ -0,0 +1,54 @@
+/*
+Copyright (c) 2016, prpl Foundation
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without 
+fee is hereby granted, provided that the above copyright notice and this permission notice appear 
+in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE
+INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE 
+FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM 
+LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, 
+ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+This code was written by Carlos Moratelli at Embedded System Group (GSE) at PUCRS/Brazil.
+
+*/
+
+/* Simple UART and Blink Bare-metal application sample using virtualized IO. */
+
+#include <arch.h>
+#include <libc.h>
+#include <hypercalls.h>
+#include <guest_interrupts.h>
+#include <platform.h>
+#include <io.h>
+
+#define RUNS 2000000
+
+volatile uint32_t milisecond_passed = 0;
+
+
+void irq_timer(){
+	milisecond_passed++;
+}
+
+long time(long *tm){
+	
+	if (tm){
+		*tm=milisecond_passed/1000;
+	}
+	
+	return milisecond_passed/1000;
+}
+
+
+int main() {
+    
+	interrupt_register(irq_timer, GUEST_TIMER_INT);
+   
+	main_dhry(RUNS);    
+	
+	return 0;
+}
+
